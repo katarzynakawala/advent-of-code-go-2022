591a6d391d477162fb131df831b62c85831e4f3f
Katarzyna Kawala <kat.kawala@gmail.com>
2022-12-04 15:02:13 +0100

    fourth day second star

--- fourthDay/main.go
+++ fourthDay/main.go
@@ -17,8 +17,7 @@ func main() {
 	doc := string(content)
 	allSections := strings.Split(doc, "\n")
 
-	var sum int
-	
+	var sumOverlap int
 
 	for _, section := range allSections {
 		twoElfs := strings.Split(section, ",")
@@ -32,53 +31,11 @@ func main() {
 
 		secondElfFirstValue, _ := strconv.Atoi(secondElfRange[0])
 		secondElfSecondValue, _ := strconv.Atoi(secondElfRange[1])
-
-		var firstElfFullRange []int
 		
-		for i := firstElfFirstValue; i <= firstElfSecondValue; i ++ {
-			firstElfFullRange = append(firstElfFullRange, i)
-		}
-
-		var secondElfFullRange []int
-		
-		for i := secondElfFirstValue; i <= secondElfSecondValue; i ++ {
-			secondElfFullRange = append(secondElfFullRange, i)
-		}
-
-		doesItContain := contains(firstElfFullRange, secondElfFullRange)
-
-		if doesItContain {
-			sum++
+		if !(firstElfSecondValue < secondElfFirstValue || secondElfSecondValue < firstElfFirstValue) {
+			sumOverlap ++
 		}
 	}
-	fmt.Println(sum)
+	fmt.Println(sumOverlap)
 	
-}
-
-func contains(s1 []int, s2 []int) bool {
-
-	var shorter []int
-	var longer []int
-	var count int 
-
-	if len(s1) < len(s2) {
-		shorter = s1
-		longer = s2
-		}else {
-		shorter = s2
-		longer = s1
-		}
-	
-    for _, a := range shorter {
-		for _, b := range longer {
-			if a == b {
-				count += 1
-			}
-		}
-    } 
-	if count == len(shorter){
-		return true
-	} else {
-		return false
-	}
 }
